be6d763a63dd45b97d061aabc80498fc595f666d
Beka.Zedginidze <Beka.Zedginidze@SPLP0525>
2024-02-20 18:39:31 +0400

    firstte

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace HR
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) 
         {
             CreateHostBuilder(args).Build().Run();
         }
